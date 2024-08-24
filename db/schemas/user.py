a3ce20e1c019e109533d079c37fd43a7aac78e93
Cristobal <quilimacox1@gmail.com>
2024-08-23 20:32:37 -0500

    Se actualiza base de datos

--- db/schemas/user.py
+++ db/schemas/user.py
@@ -2,7 +2,8 @@ def user_schemas(user) -> dict:
     return {
         "id": str(user.get("id", "")),  
         "username": user.get("username", ""),   
-        "email": user.get("email", "")
+        "email": user.get("email", ""),
+        "password": user.get("password", "")
     }
 
 def users_schemas(users) -> list:
