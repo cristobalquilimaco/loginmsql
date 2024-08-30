a5633ad91793d0dca0f2c18ea33d9675e535c9bd
Cristobal <quilimacox1@gmail.com>
2024-08-29 22:54:32 -0500

    Actualizacion

--- routers/login.py
+++ routers/login.py
@@ -27,63 +27,68 @@ class User(BaseModel):
 class UserDB(User):
     password: str
 
+users_db = {
+    
+}
+
+
 def search_user_db(username: str):
-    cursor.execute(
-        "SELECT username, email, disabled, password FROM users WHERE username = %s", 
-        (username,)
-    )
-    result = cursor.fetchone()
-    if result:
-        return UserDB(**result)
-    return None
+    if username in users_db:
+        return UserDB(**users_db[username])
+
 
 def search_user(username: str):
-    cursor.execute(
-        "SELECT username, email, disabled FROM users WHERE username = %s", 
-        (username,)
-    )
-    result = cursor.fetchone()
-    if result:
-        return User(**result)
-    return None
+    if username in users_db:
+        return User(**users_db[username])
 
-async def auth_user(token: str = Depends(oauth2_scheme)):
+
+async def auth_user(token: str = Depends(oauth2)):
+
     exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Credenciales de autenticación inválidas",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
+        headers={"WWW-Authenticate": "Bearer"})
+
     try:
         username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
         if username is None:
             raise exception
+
     except JWTError:
         raise exception
+
     return search_user(username)
+
 
 async def current_user(user: User = Depends(auth_user)):
     if user.disabled:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Usuario inactivo"
-        )
+            detail="Usuario inactivo")
+
     return user
+
 
 @router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-    user_db = search_user_db(form.username)
+
+    user_db = users_db.get(form.username)
     if not user_db:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto"
-        )
-    if not crypt.verify(form.password, user_db.password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta"
-        )
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
 
-    access_token = {
-        "sub": user_db.username,
-        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
-    }
+    user = search_user_db(form.username)
+
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+
+    access_token = {"sub": user.username,
+                    "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)}
 
     return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
