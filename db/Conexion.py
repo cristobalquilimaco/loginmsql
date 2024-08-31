fdd7b85ce775d89d5aab147765d47f41f5379900
Cristobal <quilimacox1@gmail.com>
2024-08-31 12:59:34 -0500

    Se actualiza conexion a base de datos

--- db/Conexion.py
+++ db/Conexion.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from mysql.connector import Error
 
-conexion = mysql.connector.connect(
-    user = "root",
-    password = "1234",
-    host = "localhost",
-    database = "users_db",
-    port = "3306"
+def conexion():
+    return mysql.connector.connect(
+        user = "root",
+        password = "1234",
+        host = "localhost",
+        database = "users_db",
+        port = "3306"
 )
     
 cursor = conexion.cursor(dictionary=True) 
